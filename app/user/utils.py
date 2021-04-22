4e297ddacbf24150ecaf1894ab11749c8be73327
Arowne <aaron.y.boateng@gmail.com>
2021-04-22 16:10:12 +0200

    [features] Add user utils

---
+++ app/user/utils.py
@@ -0,0 +1,17 @@
+import holidays
+import datetime
+
+from .models import User
+from .models import Login
+
+from ip2geotools.databases.noncommercial import DbIpCity
+
+def ip_localisation(ip, user):
+    response = DbIpCity.get("92.184.112.171", api_key='free')
+    
+    get_holidays = holidays.CountryHoliday(response.country) 
+    today = datetime.datetime.today()
+
+    is_holliday = today.strftime("%Y-%m-%d") in get_holidays
+    
+    Login.objects.create(user=user, location=response.country, is_holliday=is_holliday)
